db3d52e45dfc11ae52d97b246dfc779fa5bc5d66
snsluca <snsluca@gmail.com>
2020-10-14 20:55:12 +0200

    Push

--- push.bat
+++ push.bat
@@ -1,7 +1,6 @@
 git init
 git add -A
-read -e -p "Testo del commit?: " i "Push" testoCommit
-git commit -m $testoCommit
+git commit -m "Push"
 git remote add origin https://github.com/snsluca/STM.git
 git remote -v
 git push -u origin sperimentale
--- push_sperimentale.bat
+++ push_sperimentale.bat
@@ -1,8 +1,7 @@
 git init
 git checkout -b sperimentale
 git add -A
-read -e -p "Testo del commit?: " i "Push" testoCommit
-git commit -m $testoCommit
+git commit -m "Push"
 git remote add origin https://github.com/snsluca/STM.git
 git remote -v
 git push -u origin sperimentale
