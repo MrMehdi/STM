b95da8da64f0f7272729b4ff1827adf9a5185601
snsluca <snsluca@gmail.com>
2020-11-03 12:30:43 +0100

    Sperimentale

--- push.bat
+++ push.bat
@@ -1,6 +1,7 @@
 git init
 git add -A
-git commit -m "Push"
+git add C:\Users\Computer\STM32CubeIDE\workspace_1.4.0
+git commit -m "Master"
 git remote add origin https://github.com/snsluca/STM.git
 git remote -v
 git push -u origin sperimentale
--- push_sperimentale.bat
+++ push_sperimentale.bat
@@ -1,7 +1,8 @@
 git init
 git checkout -b sperimentale
 git add -A
-git commit -m "Push"
+git add C:\Users\Computer\STM32CubeIDE\workspace_1.4.0
+git commit -m "Sperimentale"
 git remote add origin https://github.com/snsluca/STM.git
 git remote -v
 git push -u origin sperimentale
