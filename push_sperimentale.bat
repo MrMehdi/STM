b9e69a3acecdf7e9fb7806dd3d0ae044de40ee31
snsluca <snsluca@gmail.com>
2020-11-07 14:10:35 +0100

    $nome

--- push_sperimentale.bat
+++ push_sperimentale.bat
@@ -5,7 +5,7 @@ git add Documentazione
 git add push.bat
 git add push_sperimentale.bat
 read -p "Inserire la descrizione breve del push: " nome
-git commit -m $nome
+git commit -m "$nome"
 git remote add origin https://github.com/snsluca/STM.git
 git remote -v
 git push -u origin sperimentale
