39ba154d258b0cec611e34a01a67c154ac925323
snsluca <snsluca@gmail.com>
2020-11-07 14:13:19 +0100

    Ottimizzazione indicatore

--- push_sperimentale.bat
+++ push_sperimentale.bat
@@ -1,11 +1,14 @@
+#!/bin/bash  
+  
 git init
 git checkout -b sperimentale
 git add LTDC_Paint
 git add Documentazione
 git add push.bat
 git add push_sperimentale.bat
-read -p "Inserire la descrizione breve del push: " nome
-git commit -m "$nome"
+
+set /p UserInputPath=Inserire la descrizione breve del push: 
+git commit -m "%UserInputPath%"
 git remote add origin https://github.com/snsluca/STM.git
 git remote -v
 git push -u origin sperimentale
